39b8fc450532ed142847b32adb7fd16ee6dd020f
Colin Nash <colinjnash@gmail.com>
2022-05-19 17:39:19 -0400

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
 
-module.exports = nextConfig
+const withMarkdoc = require('@markdoc/next.js');
+
+module.exports = withMarkdoc(/* options */)({
+  pageExtensions: ['js', 'md'],
+});
---
+++ pages/docs/concept.md
@@ -0,0 +1,8 @@
+---
+title: The Big picture
+description: This is the biggest picture
+---
+
+# Get started with Markdoc
+
+Actually, it looks pretty small.
